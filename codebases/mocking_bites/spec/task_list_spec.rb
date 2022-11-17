c68f8b5bbcae1362cdef57eaf29671129cd02a45
Michael <dev.mhowells@gmail.com>
2022-11-17 10:51:23 +0000

    completed challenge for mocking bites 1

--- codebases/mocking_bites/spec/task_list_spec.rb
+++ codebases/mocking_bites/spec/task_list_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  it "adds tasks to a list" do
+    task_list = TaskList.new
+
+    task_1 = double :task, title:"Walk the dog"
+    task_2 = double :task, title:"Walk the cat"
+
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq [task_1, task_2]
+  end
+
+  it "marks tasks as complete" do
+    task_list = TaskList.new
+    task_1 = double :task, title:"Walk the dog", complete?: true
+    task_2 = double :task, title:"Walk the cat", complete?: true
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all_complete?).to eq true
+  end
 end
